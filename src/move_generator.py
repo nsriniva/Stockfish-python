c81f50c607b59d26b715ba7b766b5f67ffa53848
Srini Nariangadu <srini.nariangadu@gmail.com>
2022-07-30 03:06:19 -0700

    Added resident memory usage check code

--- src/move_generator.py
+++ src/move_generator.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 from stockfish import UCI, Stockfish
 
+from os import getpid
+from psutil import Process
+
+process = Process(getpid())
+
 s = Stockfish()
 
 LOOP_COUNT = 1000000
 POSITION = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 
 start = datetime.now()
+start_mem = process.memory_info().rss
 for i in range(LOOP_COUNT):
     print(f'LEGAL Iteration {i}')
     s.position(POSITION)
@@ -17,7 +23,8 @@ for i in range(LOOP_COUNT):
     print(f'LEGAL : {s.legal_moves_str()}')
 
 end = datetime.now()
-
+end_mem = process.memory_info().rss
 print(f'List of legal moves for position({POSITION}) : {s.legal_moves_str()}')
 
 print(f'Time taken for {LOOP_COUNT} iterations: {end-start}')
+print(f'Change in process memory usage: {end_mem-start_mem}')
